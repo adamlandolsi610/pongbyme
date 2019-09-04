ecc52f00b36d2a471337f66fe2750b6444cb77c0
adamlandolsi610 <54667162+adamlandolsi610@users.noreply.github.com>
2019-09-04 13:35:30 +0100

    Add files via upload

---
+++ Assets/BALL.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BALL : MonoBehaviour
+{
+    public Text ScoreRightText;
+    public Text ScoreLeftText;
+    public int ScoreLeft;
+    public int ScoreRight;
+    public float speed = 30;
+    float hitFactor(Vector2 ballPosition, Vector2 racketPosision, float racketHeight)
+    {
+        return (ballPosition.y - racketPosision.y) / racketHeight;
+    }
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "player 1")
+        {
+            float y = hitFactor(transform.position,
+                     collision.transform.position,
+                     collision.collider.bounds.size.y);
+            Vector2 dir = new Vector2(-1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+       if(collision.gameObject.name == "player 2")
+        {
+            float y = hitFactor(transform.position,
+                     collision.transform.position,
+                     collision.collider.bounds.size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+       if (collision.gameObject.name =="Ysar")
+        {
+            ScoreRight ++;
+            ScoreRightText.text = ScoreRight.ToString();
+
+        }
+        if ( collision.gameObject.name=="ymin")
+        {
+            ScoreLeft ++;
+            ScoreLeftText.text = ScoreLeft.ToString();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 30;
+    public string Axis;
+    private void FixedUpdate()
+
+    {
+        float v = Input.GetAxisRaw(Axis) * speed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
